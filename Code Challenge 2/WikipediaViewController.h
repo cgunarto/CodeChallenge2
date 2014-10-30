c20c2cac810d384db92d0f49d1037f8197646945
Christina Gunarto <cgunarto@gmail.com>
2014-10-30 16:34:46 -0700

    final changes

---
+++ Code Challenge 2/WikipediaViewController.h
@@ -0,0 +1,13 @@
+//
+//  WikipediaViewController.h
+//  Code Challenge 2
+//
+//  Created by CHRISTINA GUNARTO on 10/30/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WikipediaViewController : UIViewController
+
+@end
---
+++ Code Challenge 2/WikipediaViewController.m
@@ -0,0 +1,37 @@
+//
+//  WikipediaViewController.m
+//  Code Challenge 2
+//
+//  Created by CHRISTINA GUNARTO on 10/30/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import "WikipediaViewController.h"
+
+@interface WikipediaViewController ()
+
+@end
+
+@implementation WikipediaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
